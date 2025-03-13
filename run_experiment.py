8e3569435f0ff6d85b95592ff4f145f52eaae64c
Dominik Lau <harek2388@gmail.com>
2025-03-13 12:51:28 +0100

    experiment 1: no. projections, tortosity

--- run_experiment.py
+++ run_experiment.py
@@ -3,28 +3,45 @@ import pickle
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 from time import time
 from xray_angio_3d import reconstruction
 from metrics import *
 from util import *
 
+OUTPUT_DIR="data/"
+OUTPUT_FILENAME="experiment_results.csv"
+
 def reconstruct_and_measure(gt, xinfs):
     start = time()
     reconstructed = reconstruction(xinfs)
+    elapsed_s = time() - start
     hat = np.array(reconstructed['vessel'])
     gt_vox = voxelize_points(gt)
     hat_vox = voxelize_points(hat)
-    elapsed_s = time() - start
 
     return {
         "Time [s]" : elapsed_s,
-        "Dice (3D)" : dice3d(gt_vox, hat_vox),
-        "Chamfer distance [mm]" : chamfer3d(gt_vox, hat_vox) * 1000,
-        "clDice3D" : cl_dice3d(gt_vox, hat_vox)
+        "Dice 3D [%]" : dice3d(gt_vox, hat_vox) * 100,
+        "IoU 3D [%]" : iou3d(gt_vox, hat_vox) * 100,
+        "Chamfer distance 3D [mm]" : chamfer3d(gt_vox, hat_vox) * 1000,
     }
 
 if __name__ == "__main__":
-    result = reconstruct_and_measure(*load_sample(0, "tortuous"))
-    print(result)
-    pass
+    df = []
+
+    for tortosity in ["moderate", "tortuous"]:
+        for no_projections in range(1, 16):
+            for ix in range(0, 10):
+                gt, xinfs = load_sample(ix, tortosity)
+                res = reconstruct_and_measure(gt, xinfs[:no_projections])
+                res = pd.DataFrame([{
+                        "Vessel": f"{tortosity}{ix}",
+                        "Tortosity": tortosity,
+                        "# Projections": no_projections,
+                        **res
+                    }])
+                df.append(res)    
+    result = pd.concat(df, ignore_index=True)
+    result.to_csv(os.path.join(OUTPUT_DIR, OUTPUT_FILENAME))
