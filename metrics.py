33ce9b969030373fcfe6d601469555c098fed5c2
Dominik Lau <harek2388@gmail.com>
2025-03-13 10:55:40 +0100

    added metric stubs

---
+++ metrics.py
@@ -0,0 +1,18 @@
+# 3D metrics
+
+def dice3d(gt, hat):
+    pass
+
+def cl_dice3d(gt, hat):
+    pass
+
+def chamfer_dist(gt, hat):
+    pass
+
+def iou3d(gt, hat):
+    pass
+
+# 2D metrics
+
+def dice2d(gt, hat):
+    pass
