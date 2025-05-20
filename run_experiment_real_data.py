3480dd3900ca2a266225fe5568c70279c2426ad2
Dominik Lau <harek2388@gmail.com>
2025-05-20 10:06:10 +0200

    fixup! add real data experiments

--- run_experiment_real_data.py
+++ run_experiment_real_data.py
@@ -1,5 +1,4 @@
 #!.test-env/bin/python3
-
 import os
 import pandas as pd
 import cv2
@@ -11,67 +10,76 @@ from metrics import *
 from util import *
 from vessel_tree_generator.module import *
 
-REAL_DATA_COUNT = 10
 DATA_DIR = "data/real"
-SAMPLES = [
-    ("1.3.6.1.4.1.19291.2.1.2.156249211917942617451923139", "0028.png"),
-    ("1.3.6.1.4.1.19291.2.1.2.1562492119179426174519822721", "0049.png"),
-    ("1.3.6.1.4.1.19291.2.1.2.1562492119179426174520874733", "0036.png")
-]
-INSPECTION_NAME = "o1"
-
-
-def load_format_sample(path, sample_name, filename):
-    img_path = os.path.join(path, sample_name, "masks", "vessels", filename)
-    df = pd.read_csv(os.path.join(path, "parameters.csv"), sep="\t")
-    row_df = df[df['name'] == f"\{sample_name}"]
-    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-    xinfo = XRayInfo()
-    xinfo.height = img.shape[0]
-    xinfo.width = img.shape[1]
-    xinfo.image = img
-    spacing = 0.308 / 1000,
-    xinfo.acquisition_params = {
-        'sid': row_df[[' sid']].values[0] / 1000,
-        'sod': row_df[[' sod']].values[0] / 1000,
-        'alpha': row_df[[' alfa']].values[0],
-        'beta': row_df[[' beta']].values[0],
-        "spacing_r": spacing,
-        "spacing_c": spacing
-    }
-    return xinfo
-
-
-def manual_calibration(image):
-
-    return image
-
 
 if __name__ == "__main__":
     xinfs = []
 
-    for sample_name, file_name in SAMPLES:
-        xinfo = load_format_sample(os.path.join(DATA_DIR, "o1"), sample_name, file_name)
-        xinfs.append(xinfo)
-    xinfs[2].image = manual_calibration(xinfs[2].image)
+    xinfo0 = XRayInfo()
+    xinfo0.image = cv2.imread(os.path.join(DATA_DIR, "ex6_fr14.png"), cv2.IMREAD_GRAYSCALE)
+    xinfo0.width = 512
+    xinfo0.height = 512
+    xinfo0.acquisition_params = {
+        'sid': 1.027,
+        'sod': 0.739,
+        'alpha': 22.7,
+        'beta': 19.5,
+        'spacing_r': 0.40425 / 1000,
+        "spacing_c": 0.40425 / 1000
+    }
+
+    xinfo1 = XRayInfo()
+    xinfo1.image = cv2.imread(os.path.join(DATA_DIR, "ex7_fr14.png"), cv2.IMREAD_GRAYSCALE)
+    xinfo1.width = 512
+    xinfo1.height = 512
+    xinfo1.acquisition_params = {
+        'sid': 1.200,
+        'sod': 0.749,
+        'alpha': -48.2,
+        'beta': 25.4,
+        'spacing_r': 0.40425 / 1000,
+        "spacing_c": 0.40425 / 1000
+    }
+
+    xinfo2 = XRayInfo()
+    xinfo2.image = cv2.imread(os.path.join(DATA_DIR, "ex8_fr13.png"), cv2.IMREAD_GRAYSCALE)
+    xinfo2.width = 512
+    xinfo2.height = 512
+    xinfo2.acquisition_params = {
+        'sid': 1.126,
+        'sod': 0.739,
+        'alpha': -3.4,
+        'beta': -36.3,
+        'spacing_r': 0.40425 / 1000,
+        "spacing_c": 0.40425 / 1000
+    }
+
+    xinfs = [
+        xinfo0,
+        xinfo2,
+        xinfo1,
+    ]
+
     reconstructed = reconstruction(xinfs, True)
     hat = reconstructed['vessel']
     X_hat, Y_hat, Z_hat = zip(*hat)
     sum_dice2d = 0
-    for xinfo in xinfs:
+    for i, xinfo in enumerate(xinfs):
         print(xinfo.acquisition_params)
-        spacing = xinfo.acquisition_params['spacing_r'][0]
+        spacing = xinfo.acquisition_params['spacing_r']
+        
         img_hat = make_projection(np.array(hat), 
             xinfo.acquisition_params['alpha'], xinfo.acquisition_params['beta'], 
             xinfo.acquisition_params['sod'], xinfo.acquisition_params['sid'],
             (spacing, spacing), 512
         )
         img_gt = xinfo.image
-        dilation_radius = int(np.ceil(VOXELIZATION_THRESH_DEFAULT / spacing))
+        dilation_radius = int(np.ceil(0.003 / spacing))
+        dilation_radius_gt = int(np.ceil(0.003 / spacing))
         img_hat = ndimage.binary_dilation(img_hat, structure=np.ones((dilation_radius, dilation_radius)))
-        plt.imshow(np.stack(((img_hat * 255).astype(np.uint8), img_gt, np.zeros_like(img_hat)), axis=0).T)
+        img_gt = ndimage.binary_dilation(img_gt, structure=np.ones((dilation_radius_gt, dilation_radius_gt)))
+        plt.imshow(np.stack(((img_hat * 255).astype(np.uint8), (img_gt * 255).astype(np.uint8), np.zeros_like(img_hat)), axis=0).T)
         plt.show()
-
         sum_dice2d += dice2d(img_gt, img_hat)
 
     fig = plt.figure()
